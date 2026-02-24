db8f66524f7d7d512dfc30817424c0bc2bce703f
Dhiraj0365 <dhirajchaudhary0365@gmail.com>
2026-02-24 21:54:58 +0530

    __init__.py

--- engines/__init__.py
+++ engines/__init__.py
@@ -1,1 +1,25 @@
+# engines/__init__.py
 
+"""
+Measurement & discipline-specific engines.
+
+- IS1200Engine  : Civil IS 1200 measurement rules (earthwork, RCC, plaster, etc.)
+- ElecEngine    : Electrical point wiring & cable length helpers.
+- PlumbingEngine: Plumbing pipe & fixture helpers.
+- HvacEngine    : HVAC airflow, ducting & CHW piping helpers.
+- FireEngine    : Fire-fighting (hydrant, sprinkler, alarm) helpers.
+"""
+
+from .is1200_civil import IS1200Engine
+from .elec_engine import ElecEngine
+from .plumbing_engine import PlumbingEngine
+from .hvac_engine import HvacEngine
+from .fire_engine import FireEngine
+
+__all__ = [
+    "IS1200Engine",
+    "ElecEngine",
+    "PlumbingEngine",
+    "HvacEngine",
+    "FireEngine",
+]
