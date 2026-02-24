b6d47ad661fd134fcd879b3009aee444fdcfe0ec
Dhiraj0365 <dhirajchaudhary0365@gmail.com>
2026-02-24 21:54:14 +0530

    __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,9 @@
+# data/__init__.py
 
+"""
+Data package for DSR / SoR CSV files.
+
+No runtime code is required here; this file only marks the directory as
+a Python package so you can, if needed, import data-related helpers in
+the future.
+"""
