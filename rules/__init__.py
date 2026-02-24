808f5fa6777ab84c385fbd5347104d51d274f84e
Dhiraj0365 <dhirajchaudhary0365@gmail.com>
2026-02-24 21:56:58 +0530

    __init__.py

--- rules/__init__.py
+++ rules/__init__.py
@@ -1,1 +1,22 @@
+# rules/__init__.py
 
+"""
+Rules engines for all disciplines.
+
+Exports:
+- run_all_rules          : run all discipline rules on BOQ/SOQ.
+- group_results_by_level : group RuleResult objects by ERROR/WARNING/INFO.
+- group_results_by_discipline : group RuleResult objects by discipline.
+"""
+
+from .rules_runner import (
+    run_all_rules,
+    group_results_by_discipline,
+    group_results_by_level,
+)
+
+__all__ = [
+    "run_all_rules",
+    "group_results_by_discipline",
+    "group_results_by_level",
+]
