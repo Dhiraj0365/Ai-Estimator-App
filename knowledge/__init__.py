834aa05167bb734205ad072edc28e829842023f6
Dhiraj0365 <dhirajchaudhary0365@gmail.com>
2026-02-24 21:55:50 +0530

    __init__.py

--- knowledge/__init__.py
+++ knowledge/__init__.py
@@ -1,1 +1,42 @@
+# knowledge/__init__.py
 
+"""
+Knowledge base: DSR/SoR master data & composite work packages.
+
+Exports:
+- DSR data: CPWD_BASE_DSR_2023, ITEMS, LOCATION_INDICES, PHASE_GROUPS, RATE_SOURCES
+- Civil composites: WORK_PACKAGES_CIVIL, expand_work_package
+- MEP composites  : WORK_PACKAGES_MEP, expand_mep_package
+"""
+
+from .dsr_master import (
+    CPWD_BASE_DSR_2023,
+    ITEMS,
+    LOCATION_INDICES,
+    PHASE_GROUPS,
+    RATE_SOURCES,
+    CPWD_DSR_CIVIL_2023,
+    CPWD_DSR_ELECT_2023,
+    STATE_SOR_CIVIL_2023,
+    STATE_SOR_ELECT_2023,
+)
+from .composites_civil import WORK_PACKAGES_CIVIL, expand_work_package
+from .composites_mep import WORK_PACKAGES_MEP, expand_mep_package
+
+__all__ = [
+    # DSR master
+    "CPWD_BASE_DSR_2023",
+    "ITEMS",
+    "LOCATION_INDICES",
+    "PHASE_GROUPS",
+    "RATE_SOURCES",
+    "CPWD_DSR_CIVIL_2023",
+    "CPWD_DSR_ELECT_2023",
+    "STATE_SOR_CIVIL_2023",
+    "STATE_SOR_ELECT_2023",
+    # Composites
+    "WORK_PACKAGES_CIVIL",
+    "expand_work_package",
+    "WORK_PACKAGES_MEP",
+    "expand_mep_package",
+]
